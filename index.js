417b5cf69b78c09a94f196d94cb1bd34464e44f6
ironthir <kubadzimira2002@gmail.com>
2022-02-06 16:24:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -74,9 +74,9 @@ client.on('message', receivedMessage =>{
     for(let i = 0; i < forbiddenWords.length; i++){
         if(receivedMessage.toString().includes(forbiddenWords[i])){
             receivedMessage.channel.send("<@" + receivedMessage.author.id + "> hamuj sie kurwa!")
-            receivedMessage.delete;
             if(receivedMessage.member.hasPermission('ADMINISTRATOR')) return;
             receivedMessage.member.kick();
+            receivedMessage.delete;
             return;
         }
     }

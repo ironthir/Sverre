86c2ccaf699ba85c32e3a0442cf111869d6107ad
ironthir <kubadzimira2002@gmail.com>
2022-02-20 00:24:10 +0100

    amaro

---
+++ commands/amaro.js
@@ -0,0 +1,6 @@
+module.exports = {
+	name: 'amaro',
+	description: 'amaroCurrent weather status for your city',
+	execute(receivedMessage, arguments) {
+		receivedMessage.channel.send("Wojciech Modest Amaro z domu Basiura, jedyny w Polsce zdobywca prestiżowej gwiazdki Michelin, którą zdobył po 10 latach pracy za granicą pod okiem samego Gordona Ramsaya jako polski kucharz używający polskich produktów na polskiej ziemi. Pracował przez 8 miesiecy 7 dni w tygodniu po 12h");
+}}
